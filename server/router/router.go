c6e716e57cc8610c7a8e0677ccb3c4677092a1bb
Unanoc <leedaniil98@gmail.com>
2018-12-28 15:38:57 +0300

    added example

--- server/router/router.go
+++ server/router/router.go
@@ -9,9 +9,9 @@ import (
 func NewRouter() *fasthttprouter.Router {
 	router := fasthttprouter.New()
 
-	router.GET("/api/products/product/:product_id/", handlers.GetProductByIdHandler) // получение определенного продукта по product_id
-	router.GET("/api/products/all", handlers.GetProductsAllHandler) // получение всех продуктов
-	router.GET("/api/products/status", handlers.GetProductsStatusHandler) // получение всех продуктов
+	router.GET("/api/products/product/:product_id/", handlers.GetProductByIdHandler) // получение определенного продукта по product_id 
+	router.GET("/api/products/all", handlers.GetProductsAllHandler) // получение всех продуктов (http://localhost:3000/api/products/all?limit=10&from=100&to=1000)
+	router.GET("/api/products/status", handlers.GetProductsStatusHandler) // получение информации о количество продуктов в бд
 	router.POST("/api/products/product/", handlers.PostProductByIdHandler) // создание продукта
 
 	return router
